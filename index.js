9377d73ea592013216699c4f0016cabf2684e9e5
Reed James <reedtjames1234@gmail.com>
2019-08-08 15:31:50 -0500

    Added code for Heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // code away!
+require("dotenv").config();
 const server = require("./server.js");
 
-const port = 8000;
+const port = process.env.PORT;
 server.listen(port, () =>
   console.log(`\n Lok'tar on http://localhost:${port}\n`)
 );
