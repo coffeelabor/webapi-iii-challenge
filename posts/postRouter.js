d018761e04e888fc8ce39c4b47c24fad08465fdc
Reed James <reedtjames1234@gmail.com>
2019-08-09 09:50:00 -0500

    Final commit before switching projects

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -39,7 +39,18 @@ router.delete("/:id", validatePostId, (req, res) => {
     });
 });
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", validatePostId, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  postDb
+    .update(id, changes)
+    .then(update => {
+      res.status(200).json(update);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "You know the drill, this is not good" });
+    });
+});
 
 // custom middleware
 
@@ -64,6 +75,11 @@ function validatePostId(req, res, next) {
           "O bummer, either its an internal server error or the end of the world"
       });
     });
+}
+
+function validatePost(req, res, next) {
+  const body = req.body;
+  const text = req.body.text;
 }
 
 module.exports = router;
